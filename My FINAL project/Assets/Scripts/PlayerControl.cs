57c79ede7a9ced2d1ade2fe3971815f14fa47a4e
alonsotorres042 <143733836+alonsotorres042@users.noreply.github.com>
2023-12-01 00:10:24 -0500

    player input advance (ya funciona mas o menos)

--- My FINAL project/Assets/Scripts/PlayerControl.cs
+++ My FINAL project/Assets/Scripts/PlayerControl.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    private Rigidbody _myRB;
+    public Vector3 Speed; 
     // Start is called before the first frame update
     void Start()
     {
-
+        _myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        _myRB.velocity = Speed;
     }
-    void OnMovement()
+    public void OnMovement(InputAction.CallbackContext context) 
     {
-
+        Vector3 moveInput = context.ReadValue<Vector3>();
+        Speed = moveInput * 2;
+        Debug.Log(moveInput);
     }
 }
