f0e879320d1a0fc47ce80039b8adc8e7dd95fccf
alonsotorres042 <143733836+alonsotorres042@users.noreply.github.com>
2023-12-01 12:20:45 -0500

    The JUMP Update is here¡¡

--- My FINAL project/Assets/Scripts/PlayerControl.cs
+++ My FINAL project/Assets/Scripts/PlayerControl.cs
@@ -8,21 +8,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    private Rigidbody _myRB;
-    private Transform _transform;
+    public int AvailableJumps = 0;
+    private bool _canJump;
     public Vector3 VectorSpeed;
+    public Transform _transform;
+    public Rigidbody _myRB;
     public float CircularSpeed;
     public float SpeedValue;
     private float _counter;
+    public float Thrust;
     public float Width;
     public float Depth;
     // Start is called before the first frame update
     void Start()
     {
+        _canJump = true;
+        Thrust = 300f;
         Width = 10;
         Depth = 10;
         _counter = 0;
-        SpeedValue = 5;
+        SpeedValue = 1;
         CircularSpeed = 0;
         _myRB = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
@@ -33,11 +38,15 @@ public class PlayerControl : MonoBehaviour
     {
         //_myRB.velocity = VectorSpeed;        <------   LIBERAR EN CASO DE QUERER MOVIMIENTO X & Z NORMALES.
         _counter += Time.deltaTime * CircularSpeed;
-
         float x = Mathf.Cos(_counter) * Width;
         float z = Mathf.Sin(_counter) * Depth;
 
         _transform.position = new Vector3(x, _transform.position.y, z);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        //NO OLVIDAR: Colocar un if para comparar los tags¡¡ NO OLVIDAR
+        _canJump = true;
     }
     public void OnMovement(InputAction.CallbackContext context) 
     {
@@ -55,6 +64,16 @@ public class PlayerControl : MonoBehaviour
         {
             CircularSpeed = 0;
         }
-        Debug.Log(PlayerInput);
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //Vector3 jumpInput = context.ReadValue<Vector3>();
+        if(context.performed)
+        {
+            if(_canJump == true)
+            {
+                _myRB.AddForce(_transform.up * Thrust);
+            }
+        }
     }
 }
